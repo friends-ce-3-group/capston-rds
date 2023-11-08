85c117c77a09e5de0ef3e39ad6322c71cb85091c
Low Chee Meng <lowcheemeng02@gmail.com>
2023-11-08 12:26:01 +0800

    reorg files

---
+++ testing/main.tf
@@ -0,0 +1,33 @@
+module "rds" {
+    source = "../"
+
+    region = var.region
+
+    proj_name = var.proj_name
+
+    proj_name_root = var.proj_name_root
+
+    db_name = var.db_name
+
+    db_allocated_storage = var.db_allocated_storage
+
+    db_instance_class = var.db_instance_class
+
+    db_storage_type = var.db_storage_type
+
+    db_engine = var.db_engine
+
+    db_engine_version = var.db_engine_version # use the tried and tested mysql:5.7
+
+    db_username = var.db_username
+
+    db_password = var.db_password
+
+    db_backup_window = var.db_backup_window
+
+    db_maintenance_window = var.db_maintenance_window
+
+    db_backup_retention_period = var.db_backup_retention_period
+
+    db_monitoring_interval = var.db_monitoring_interval # seconds, min 60s
+}
---
+++ testing/variables.tf
@@ -0,0 +1,59 @@
+variable "proj_name" {
+  type = string
+}
+
+variable "proj_name_root" {
+  type = string
+}
+
+variable "region" {
+  type = string
+}
+
+variable "db_name" {
+  type = string
+}
+
+variable "db_allocated_storage" {
+  type = number
+}
+
+variable "db_instance_class" {
+  type = string
+}
+
+variable "db_storage_type" {
+  type = string
+}
+
+variable "db_engine" {
+  type = string
+}
+
+variable "db_engine_version" {
+  type = string
+}
+
+variable "db_username" {
+  type = string
+}
+
+variable "db_password" {
+  type = string
+}
+
+variable "db_backup_window" {
+  type = string
+}
+
+variable "db_maintenance_window" {
+  type = string
+}
+
+variable "db_backup_retention_period" {
+  type = number
+}
+
+variable "db_monitoring_interval" {
+  type = number
+}
