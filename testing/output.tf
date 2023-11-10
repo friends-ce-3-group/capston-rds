bcb0b67e5ee61fa1a1270166deeb41bd768003de
Low Chee Meng <lowcheemeng02@gmail.com>
2023-11-10 15:17:51 +0800

    update again

--- testing/output.tf
+++ testing/output.tf
@@ -1,4 +1,4 @@
 resource "local_file" "write_url" {
-  content  = "RDS_ADDRESS=${module.rds.db_endpoint}"
+  content  = "RDS_ADDRESS_URL=${module.rds.db_endpoint}"
   filename = "${path.module}/RDS_ADDRESS.dat"
 }
